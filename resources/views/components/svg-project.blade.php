bce7b5cae541c85b132d9bd3c8b6dd0f181f1a8a
Elena Khramova <58597649+purplena@users.noreply.github.com>
2024-07-25 15:56:24 +0200

    poc: blob generator for front design (#15)

---
+++ resources/views/components/svg-project.blade.php
@@ -0,0 +1,4 @@
+<svg class="h-full w-full" viewBox="0 0 88 88" xmlns="http://www.w3.org/2000/svg">
+    <path transform="translate(44 44)" fill="#FDF8EE" d="{{ $coordinates }}">
+    </path>
+</svg>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
     <div class="mt-navbarMargin">
         <x-sections.hero />
         <x-sections.about />
-        <x-sections.projects />
+        <x-sections.projects :coordinates="$coordinates" />
         <x-sections.contact />
     </div>
 </x-layout>
