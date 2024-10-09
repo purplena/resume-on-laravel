d95a055c341541984e57b37eb2f357ec1fb11e6d
purplena <lkhramova14@gmail.com>
2024-10-09 11:55:34 +0200

    hot-fix: added to respective thems element with background "white"

--- resources/views/components/sections/components/language-switcher.blade.php
+++ resources/views/components/sections/components/language-switcher.blade.php
@@ -6,7 +6,7 @@
                     class="uppercase text-h6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-main-700">{{ $available_locale }}</span>
             </div>
         @else
-            <a class="social-link-transition block cursor-pointer w-[28px] h-[28px] bg-white rounded-full border border-solid border-main-500 relative text-main-500"
+            <a class="language-switcher-link social-link-transition block cursor-pointer w-[28px] h-[28px] rounded-full border border-solid border-main-500 relative text-main-500"
                 href={{ route('language', ['locale' => $available_locale]) }}>
                 <span class="uppercase text-h6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     {{ $available_locale }}
